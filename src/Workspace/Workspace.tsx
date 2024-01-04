731560210691dc1b99192756c3482edf051f6d8d
Oleg <olegpl1993@gmail.com>
2024-01-04 03:01:30 +0200

    feat: scale

--- src/Workspace/Workspace.tsx
+++ src/Workspace/Workspace.tsx
@@ -9,11 +9,12 @@ interface Props {
   filters: Filters;
   imgUrl: string | null;
   stageRef: React.RefObject<Konva.Stage>;
+  scale: number;
 }
 
 function Workspace(props: Props) {
   console.log("workspace");
-  const { filters, imgUrl, stageRef } = props;
+  const { filters, imgUrl, stageRef, scale } = props;
   const imageRef = useRef<Konva.Image>(null);
   const [image] = useImage(imgUrl || "/dragon.jpg", "anonymous");
 
@@ -31,34 +32,46 @@ function Workspace(props: Props) {
 
   return (
     <div className={styles.workspace}>
-      <Stage
-        width={image.width}
-        height={image.height}
-        style={{
-          padding: "20px",
-          display: "inline-flex",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "",
-          minWidth: "100%",
-          minHeight: "100%",
-        }}
-        ref={stageRef}
-      >
-        <Layer>
-          <Image
-            ref={imageRef}
-            image={image!}
-            width={image.width}
-            height={image.height}
-            x={0}
-            y={0}
-            filters={[Konva.Filters.Blur]}
-          />
-          <Rect width={100} height={100} fill="blue" draggable />
-          <Rect width={100} height={100} fill="red" x={200} y={200} draggable />
-        </Layer>
-      </Stage>
+      <div className={styles.wrapper}>
+        <Stage
+          width={image.width}
+          height={image.height}
+          ref={stageRef}
+          style={{
+            padding: "20px",
+            display: "inline-flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "",
+            minWidth: "100%",
+            minHeight: "100%",
+            transform: `scale(${scale})`,
+          }}
+        >
+          <Layer>
+            <Image
+              ref={imageRef}
+              image={image!}
+              width={image.width}
+              height={image.height}
+              x={0}
+              y={0}
+              filters={[Konva.Filters.Blur]}
+            />
+
+            <Rect width={100} height={100} fill="blue" draggable />
+
+            <Rect
+              width={100}
+              height={100}
+              fill="red"
+              x={200}
+              y={200}
+              draggable
+            />
+          </Layer>
+        </Stage>
+      </div>
     </div>
   );
 }
