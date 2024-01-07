068c4668d45138e3fabfdbbe40b0f9828af01e27
Oleg <olegpl1993@gmail.com>
2024-01-08 00:57:18 +0200

    feat: filters update main img

--- src/Toolbar/FiltersToolbar/FiltersToolbar.tsx
+++ src/Toolbar/FiltersToolbar/FiltersToolbar.tsx
@@ -4,12 +4,12 @@ import styles from "./FiltersToolbar.module.css";
 interface Props {
   filters: Filters;
   setFilters: React.Dispatch<React.SetStateAction<Filters>>;
-  isFiltersOpen: boolean;
   setIsFiltersOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  updateFiltersImage: () => void;
 }
 
 function FiltersToolbar(props: Props) {
-  const { filters, setFilters, isFiltersOpen, setIsFiltersOpen } = props;
+  const { filters, setFilters, setIsFiltersOpen, updateFiltersImage } = props;
 
   const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -19,49 +19,68 @@ function FiltersToolbar(props: Props) {
     }));
   };
 
-  if (isFiltersOpen) {
-    return (
-      <div className={styles.filtersToolbar}>
-        <div className={styles.row}>
-          <p className={styles.label}>Blur</p>
-          <input
-            type="range"
-            min="0"
-            max="100"
-            name="blur"
-            className={styles.input}
-            value={filters.blur}
-            onChange={handleFilterChange}
-          />
-          <p className={styles.value}>{filters.blur}</p>
-        </div>
+  const handleFilterReset = () => {
+    const copyFilters = { ...filters };
+    Object.keys(copyFilters).forEach((key) => {
+      copyFilters[key as keyof Filters] = 0;
+    });
+    setFilters(copyFilters);
+  };
 
-        <div className={styles.row}>
-          <p className={styles.label}>Brighten</p>
-          <input
-            type="range"
-            min="-1"
-            max="1"
-            step="0.01"
-            name="brighten"
-            className={styles.input}
-            value={filters.brighten}
-            onChange={handleFilterChange}
-          />
-          <p className={styles.value}>{filters.brighten}</p>
-        </div>
+  const handleFiltersApply = () => {
+    updateFiltersImage();
+    handleFilterReset();
+  };
 
-        <button
-          className={styles.btn}
-          onClick={() => {
-            setIsFiltersOpen(false);
-          }}
-        >
-          Apply
-        </button>
+  const handleFiltersClose = () => {
+    setIsFiltersOpen(false);
+    handleFilterReset();
+  };
+
+  return (
+    <div className={styles.filtersToolbar}>
+      <div className={styles.row}>
+        <p className={styles.label}>Blur</p>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          name="blur"
+          className={styles.input}
+          value={filters.blur}
+          onChange={handleFilterChange}
+        />
+        <p className={styles.value}>{filters.blur}</p>
       </div>
-    );
-  }
+
+      <div className={styles.row}>
+        <p className={styles.label}>Brighten</p>
+        <input
+          type="range"
+          min="-1"
+          max="1"
+          step="0.01"
+          name="brighten"
+          className={styles.input}
+          value={filters.brighten}
+          onChange={handleFilterChange}
+        />
+        <p className={styles.value}>{filters.brighten}</p>
+      </div>
+
+      <button className={styles.btn} onClick={handleFilterReset}>
+        Reset
+      </button>
+
+      <button className={styles.btn} onClick={handleFiltersClose}>
+        Close
+      </button>
+
+      <button className={styles.btn} onClick={handleFiltersApply}>
+        Apply
+      </button>
+    </div>
+  );
 }
 
 export default FiltersToolbar;
